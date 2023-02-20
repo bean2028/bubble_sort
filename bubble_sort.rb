6c25511e87e4c25d18f0483e7736a34195626b03
Eric Helstad <erichelstad@gmail.com>
2023-02-20 14:10:27 -0600

    Loop over items and swap if out of order

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,5 +3,17 @@ require 'pry-byebug'
 number_list = [4,3,78,2,0,2]
 
 def bubble_sort(array)
-
+  size = array.length
+  swapped = nil
+  until swapped == false do
+    swapped = false
+    for i in 0..(size - 1)
+      if array[i - 1] > array[i]
+        array[i - 1], array[i] = array[i], array[i - 1]
+        binding.pry
+      end
+    end
+  end
 end
+
+bubble_sort(number_list)
