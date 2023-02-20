20652da88362cf0b3244356650c50e6c8b98c5e4
Eric Helstad <erichelstad@gmail.com>
2023-02-20 14:37:13 -0600

    Loop over all items until sorted

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,20 @@
 require 'pry-byebug'
 
-number_list = [4,3,78,2,0,2]
+number_list = [4,3,78,2]
 
 def bubble_sort(array)
   size = array.length
   swapped = nil
   until swapped == false do
     swapped = false
-    for i in 0..(size - 1)
+    for i in 1..(size - 1)
       if array[i - 1] > array[i]
         array[i - 1], array[i] = array[i], array[i - 1]
-        binding.pry
+        swapped = true
       end
     end
   end
+  puts array
 end
 
 bubble_sort(number_list)
