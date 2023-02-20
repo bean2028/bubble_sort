b76ba0fbcd425d6bd47234a3267581156b379539
Eric Helstad <erichelstad@gmail.com>
2023-02-20 14:43:17 -0600

    Add logic to handle sorting items equal in value

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,6 @@
 require 'pry-byebug'
 
-number_list = [4,3,78,2,0,100,23,6,25]
+number_list = [4,3,78,2,0,100,23,6,23]
 
 def bubble_sort(array)
   size = array.length
@@ -8,7 +8,7 @@ def bubble_sort(array)
   until swapped == false do
     swapped = false
     for i in 1..(size - 1)
-      if array[i - 1] > array[i]
+      if array[i - 1] >= array[i]
         array[i - 1], array[i] = array[i], array[i - 1]
         swapped = true
       end
