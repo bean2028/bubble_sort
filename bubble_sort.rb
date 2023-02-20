c97a0f2f518f35823b34aa4c615093b5a080fa2d
Eric Helstad <erichelstad@gmail.com>
2023-02-20 14:40:59 -0600

    Optimize algorithm by not looping over items already in correct order

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,6 @@
 require 'pry-byebug'
 
-number_list = [4,3,78,2]
+number_list = [4,3,78,2,0,100,23,6,25]
 
 def bubble_sort(array)
   size = array.length
@@ -13,8 +13,9 @@ def bubble_sort(array)
         swapped = true
       end
     end
+    size = size - 1
   end
-  puts array
+  p array
 end
 
 bubble_sort(number_list)
